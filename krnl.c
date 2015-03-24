71eb5e1cfc59b960bd6c23c2caa6c7646172ffef
Jens Dalsgaard Nielsen <jdn@es.aau.dk>
2015-03-24 16:41:02 +0100

    tmr parked

--- krnl.c
+++ krnl.c
@@ -46,13 +46,15 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-
+/*
 #if F_CPU == 16000000L
 #define SCL 1
  #elif F_CPU == 8000000L
-#define SCL  0.5
+#define SCL  2
  #endif
-
+ until further notice
+*/
+#define SCL 1
 //#if (MHZ == F16)
 //#elif (MHZ == F08)
 //#endif
@@ -986,7 +988,7 @@ k_start (int tm)
     // will not start if errors during initialization
         if (k_err_cnt)
             return -k_err_cnt;
-
+    tm /= SCL;
     // boundary check
         if (tm <= 0)
             return -555;
@@ -1010,7 +1012,7 @@ k_start (int tm)
     TCCRxA = 0;
     TCCRxB = PRESCALE; // atm328s  2560,...
 
-    tcntValue = COUNTMAX  - SCL*tm*DIVV; // SCL fo 8 MHz versions
+    tcntValue = COUNTMAX  - tm*DIVV; // SCL fo 8 MHz versions
     TCNTx = tcntValue;
 
     //  let us start the show
